41ec6c0059fa0e201c91d9fcbab6f700bcea9250
bobbern <robertbernier@gmail.com>
2018-08-02 09:52:38 -0400

    du laptop 2 aout 2018 enlever init exit also ..

--- app/src/main/java/ca/robertbernier/exercices/appbobgeosilent/MainActivity.java
+++ app/src/main/java/ca/robertbernier/exercices/appbobgeosilent/MainActivity.java
@@ -324,8 +324,7 @@ public class MainActivity extends AppCompatActivity
         // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
         // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
         // is already inside that geofence.
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER |
-                GeofencingRequest.INITIAL_TRIGGER_EXIT );
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER );
 
         // Add the geofences to be monitored by geofencing service.
         builder.addGeofences(mGeofenceList);
